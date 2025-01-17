d95638a4a5d89523e5f8003bd04a0470ab2868e9
GozdeEBaran <eski.gozdee@gmail.com>
2025-01-17 09:43:23 -0500

    lab1 is added in lab1 folder

---
+++ lab1/lab1.py
@@ -0,0 +1,26 @@
+# Sample Coding Questions 01 Week 01
+# Gozde Baran
+# 101515982
+
+##########################################################
+x = 1
+y = 4
+z = 7
+q = 9
+
+##########################################################
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - (b ** c) // d) + a % c
+print(e)
+
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+##########################################################
+userAge = input("Enter your age: ")
+print(f"Now showing the shop items filtered by age: {userAge}")
