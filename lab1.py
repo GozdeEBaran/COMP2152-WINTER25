584ae16c3ac841f53148e1349c522030372d66fa
GozdeEBaran <eski.gozdee@gmail.com>
2025-01-17 09:31:32 -0500

    lab1 done

---
+++ lab1.py
@@ -0,0 +1,28 @@
+# Sample Coding Questions 01 Week 01
+# Gozde Baran
+# 101515982
+
+##########################################################
+x=1
+y=4
+z=7
+q=9
+
+##########################################################
+
+a=1
+b=2
+c=3
+d=4
+
+e=(a-(b**c)//d)+a%c
+print(e)
+
+temparature= 32.6
+print("the temprature today is : {temparature}.3f degrees Celcius")
+
+##########################################################
+userAge= input("Enter your age:")
+print("Now showing the shop items filtered by age : {userAge} "
+
+##########################################################
